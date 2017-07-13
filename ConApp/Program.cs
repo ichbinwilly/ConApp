b841ff248376a9ec7952769c45aacb8e4c9fff31
ichbinwilly <ichbinwilly>
2017-07-13 11:18:25 +0800

    1. test

--- ConApp/Program.cs
+++ ConApp/Program.cs
@@ -11,7 +11,7 @@ namespace ConApp
 {
     class Program
     {
-        /* EntityFramework 4.3 CodeFirst Trivial One File Example – Part 1
+        /* EntityFramework 4.3 CodeFirst Trivial One File Example – Part 1 test
          * Reference1: http://peterkellner.net/2012/02/17/entityframework-4-3-codefirst-trivial-one-file-example-part-1/
          * Reference2: http://peterkellner.net/2012/02/17/entityframework-codefirst-4-3-adding-data-migration-to-simple-example-part-2
          * Reference3: http://peterkellner.net/2012/02/17/entityframework-code-first-4-3-adding-a-single-default-column-to-a-migration-enabled-project-part-3
