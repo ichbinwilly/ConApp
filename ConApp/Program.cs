0f6713a11146fae95b08d747f9744bde17a8226d
ichbinwilly <ichbinwilly>
2017-07-13 11:25:40 +0800

    1. Edit the README.md and add the references 2. Remove the references in Program.cs

--- ConApp/Program.cs
+++ ConApp/Program.cs
@@ -11,11 +11,6 @@ namespace ConApp
 {
     class Program
     {
-        /* EntityFramework 4.3 CodeFirst Trivial One File Example – Part 1 test
-         * Reference1: http://peterkellner.net/2012/02/17/entityframework-4-3-codefirst-trivial-one-file-example-part-1/
-         * Reference2: http://peterkellner.net/2012/02/17/entityframework-codefirst-4-3-adding-data-migration-to-simple-example-part-2
-         * Reference3: http://peterkellner.net/2012/02/17/entityframework-code-first-4-3-adding-a-single-default-column-to-a-migration-enabled-project-part-3
-         */
         static void Main(string[] args)
         {
             Database.SetInitializer<SiteDb>(new SiteDbInitialize());
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ConApp
-First glance of EntityFramework 4.3 CodeFirst
+First glance of EntityFramework using CodeFirst
+
+EntityFramework 4.3 CodeFirst Trivial One File Example – Part 1 test
+
+	Reference1: http://peterkellner.net/2012/02/17/entityframework-4-3-codefirst-trivial-one-file-example-part-1/
+	Reference2: http://peterkellner.net/2012/02/17/entityframework-codefirst-4-3-adding-data-migration-to-simple-example-part-2
+	Reference3: http://peterkellner.net/2012/02/17/entityframework-code-first-4-3-adding-a-single-default-column-to-a-migration-enabled-project-part-3
+	
+         
